5e43f11c47142a03ca56a68fa2cf9343a78759a3
hui <792717352@qq.com>
2019-03-08 17:40:29 +0800

    sql

--- sql/grade_manage_system.sql
+++ sql/grade_manage_system.sql
@@ -110,48 +110,81 @@ CREATE TABLE `gm_student`  (
 -- Table structure for gm_teacher
 -- ----------------------------
 DROP TABLE IF EXISTS `gm_teacher`;
-CREATE TABLE `gm_teacher`  (
-                             `id` int(11) NOT NULL AUTO_INCREMENT,
-                             `workNo` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '工号',
-                             `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '姓名',
-                             `loginId` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '登陆账号',
-                             `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '登陆密码',
-                             `sex` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '性别',
-                             `birthday` datetime(0) DEFAULT NULL COMMENT '出生日期',
-                             `hiredate` datetime(0) DEFAULT NULL COMMENT '入职日期',
-                             `position` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '职称',
-                             `phone` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '联系电话',
-                             `createAt` datetime(0) DEFAULT NULL,
-                             `updateAt` datetime(0) DEFAULT NULL,
-                             `role` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '角色，班主任classTeacher，系统管理员sysAdmin,教务管理员eduAdmin，教学领导teachLeader',
-                             `isDelete` int(11) DEFAULT NULL COMMENT '是否删除，1：删除，0：未删除',
-                             `classId` int(11) COMMENT '班级Id',
-                             PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+create table gm_teacher
+(
+  id       int auto_increment
+    primary key,
+  workNo   varchar(50)  null comment '工号',
+  name     varchar(100) null comment '姓名',
+  loginId  varchar(50)  not null comment '登陆账号',
+  password varchar(50)  null comment '登陆密码',
+  sex      varchar(11)  null comment '性别',
+  birthday datetime     null comment '出生日期',
+  hiredate datetime     null comment '入职日期',
+  position varchar(50)  null comment '职称',
+  phone    varchar(50)  null comment '联系电话',
+  createAt datetime     null,
+  updateAt datetime     null,
+  role     varchar(500)  null comment '角色，超级管理员为superAdmin，其他为角色id（多个用"，"隔开）,班主任classTeacher，系统管理员sysAdmin,教务管理员eduAdmin，教学领导teachLeader',
+  isDelete int          null comment '是否删除，1：删除，0：未删除',
+  classId  int          null comment '班级Id'
+)
+  charset = utf8;
+
 --超管账号
 insert into gm_teacher(loginId, password,role) values ('admin', 'admin','superAdmin');
+
+
 -- ----------------------------
 -- Table structure for user_module
 -- ----------------------------
 DROP TABLE IF EXISTS `user_module`;
-CREATE TABLE `user_module`  (
-                              `id` int(11) NOT NULL AUTO_INCREMENT,
-                              `moduleName` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '模块名称',
-                              `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '路径',
-                              PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+create table user_module
+(
+  id         int auto_increment
+    primary key,
+  moduleName varchar(50)  null comment '模块名称',
+  url        varchar(255) null comment '路径',
+  pid        int          null comment '父模块'
+)
+  charset = utf8;
+
 
 -- ----------------------------
 -- Table structure for user_role
 -- ----------------------------
 DROP TABLE IF EXISTS `user_role`;
-CREATE TABLE `user_role`  (
-                            `id` int(11) NOT NULL AUTO_INCREMENT,
-                            `roleName` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '角色名',
-                            `moduleIds` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '模块Ids',
-                            PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+create table user_role
+(
+  id         int auto_increment
+    primary key,
+  role       varchar(50)  not null comment '角色代码',
+  roleName   varchar(50)  null comment '角色名',
+  moduleIds  varchar(500)  null comment '模块Ids',
+  moduleUrls varchar(500) null comment '模块路径'
+)
+  charset = utf8;
 
-SET FOREIGN_KEY_CHECKS = 1;
+
+
+DROP TABLE IF EXISTS `role_module_relation`;
+create table role_module_relation
+(
+  id       int auto_increment
+    primary key,
+  roleId   int null,
+  moduleId int null
+);
+DROP TABLE IF EXISTS `teacher_role_relation`;
+create table teacher_role_relation
+(
+  id        int auto_increment
+    primary key,
+  teacherId int null,
+  roleId    int null
+);
+
+
+
 
 
